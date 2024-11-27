9ba84695c2c1706d9c2ccc8b02fac9317afad64d
ibenito <73246581+benitoexe@users.noreply.github.com>
2024-11-27 12:24:21 -0700

    Add files via upload

--- playlist.py
+++ playlist.py
@@ -1,29 +1,45 @@
-
 songs = ["A", "B", "C", "D", "E"]
 
+#button 1 moves the first song to the end
 def button1():
-    song.append(songs.pop(0))
+    songs.append(songs.pop(0))
+
+#button 2 moves the last song to the beginning 
 def button2():
-    songs.insert(songs.pop(-1))
+    songs.insert(0, songs.pop(-1))
+
+#button 3 shuffles the playlist
+import random
 def button3():
-    pass
+    random.shuffle(songs)
+    print("Playlist shuffled!")
+
+#button 4 restores the order of the playlist
+original_songs = ["A", "B", "C", "D", "E"]
 def button4():
-    pass
+    global songs
+    songs = original_songs[:]
+    print("Playlist restarted!")
+
+#button 5 shows all songs in the playlist
 def button5():
+    print("\nCurrent Playlist:")
     for song in songs:
         print(song)
 
-print("""enter a number b/w 1 and 5
-1: will move the ...
-2: will ...
-3: will ...
-4: will ...
-you can repeat 1 through 4 as many times as you like,
-5 will play the songs in the...""")
+#printing menu instructions
+print("""Enter a number between 1 and 5:
+1: Move the first song to the end
+2: Move the last song to the beginning
+3: Shuffle the playlist
+4: Restart the playlist
+5: Display the playlist and exit
+""")
 
+#loop 
 option = ''
-
 while option != '5':
+    option = input("Choose an option (1-5): ").strip()
     if option == '1':
         button1()
     elif option == '2':
@@ -33,7 +49,7 @@ while option != '5':
     elif option == '4':
         button4()
     elif option == '5':
+        button5()
         break
     else:
-        print("invalid input :( try again")
-        
+        print("Invalid input. Please try again.")
